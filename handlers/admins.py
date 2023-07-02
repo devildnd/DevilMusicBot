950f60c943e6715653156d297d731c73005a40be
WHITEHELL097 <101397767+WHITEHELL097@users.noreply.github.com>
2023-07-02 23:42:36 +0530

    Add files via upload

---
+++ handlers/admins.py
@@ -0,0 +1,120 @@
+from asyncio import QueueEmpty
+from cache.admins import set
+from pyrogram import Client
+from pyrogram.types import Message
+from callsmusic import callsmusic
+from queues import queues
+import traceback
+import os
+import sys
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputStream
+from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram import filters, emoji
+from config import BOT_USERNAME as BN
+from helpers.filters import command, other_filters
+from helpers.decorators import errors, authorized_users_only
+from config import que, admins as a
+
+ACTV_CALLS = []
+
+@Client.on_message(filters.command('reload'))
+async def update_admin(client, message):
+    global a
+    admins = await client.get_chat_members(message.chat.id, filter="administrators")
+    new_ads = []
+    for u in admins:
+        new_ads.append(u.user.id)
+    a[message.chat.id] = new_ads
+    await message.reply_text('Sucessfully updated admin list in **{}**'.format(message.chat.title))
+
+
+@Client.on_message(command("pause") & other_filters)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    chat_id = message.chat.id
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is playing!")
+    else:
+        await callsmusic.pytgcalls.pause_stream(chat_id)
+        await message.reply_text("▶️ Paused!")
+
+
+@Client.on_message(command("resume") & other_filters)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    chat_id = message.chat.id
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is paused!")
+    else:
+        await callsmusic.pytgcalls.resume_stream(chat_id)
+        await message.reply_text("⏸ Resumed!")
+
+
+@Client.on_message(command("end") & other_filters)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    chat_id = message.chat.id
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is streaming!")
+    else:
+        try:
+            queues.clear(message.chat.id)
+        except QueueEmpty:
+            pass
+
+        await callsmusic.pytgcalls.leave_group_call(chat_id)
+        await message.reply_text("❌ Stopped streaming!")
+
+
+@Client.on_message(command("skip") & other_filters)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    global que
+    chat_id = message.chat.id
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Kuch bja toh le skip krne k liye!")
+    else:
+        queues.task_done(chat_id)
+
+        if queues.is_empty(chat_id):
+            await callsmusic.pytgcalls.leave_group_call(chat_id)
+        else:
+            await callsmusic.pytgcalls.change_stream(
+                chat_id,
+                InputStream(
+                    InputAudioStream(
+                        queues.get(chat_id)["file"],
+                    ),
+                ),
+            )
+                
+
+    qeue = que.get(chat_id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f'- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**')
+
+
+@Client.on_message(
+    filters.command("reload")
+)
+@errors
+async def admincache(client, message: Message):
+    set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
+     #await message.reply_text("✯𝗩𝗖𝗣𝗹𝗮𝘆𝗕𝗼𝘁✯=❇️ Admin cache refreshed!")
